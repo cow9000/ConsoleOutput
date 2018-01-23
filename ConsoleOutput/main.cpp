8d4df140ce1b39791cd2275954c0055a890b39ee
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-01-23 13:06:15 -0700

    Initial commit

---
+++ ConsoleOutput/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConsoleOutput
+//
+//  Created by Vawdrey, Derek on 1/23/18.
+//  Copyright © 2018 Vawdrey, Derek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
